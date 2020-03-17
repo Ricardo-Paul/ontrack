0da3dd5fddd57194aa2b63dc5e98d3edf4e3d6a2
Ricardo-Paul <ricardopaul459@gmail.com>
2020-03-17 10:43:13 -0400

    remove dev tool

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk  from 'redux-thunk'
 import { dayReducer } from './dayReducer'
 
-
-const composedEnhancers = compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composedEnhancers = compose(applyMiddleware(thunk))
 
 const store = createStore(dayReducer, composedEnhancers)
 
